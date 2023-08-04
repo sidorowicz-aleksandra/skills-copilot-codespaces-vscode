c96e88bb5a6733d63ac10ac6b75fdaa924d20498
sidorowicz-aleksandra <43880743+sidorowicz-aleksandra@users.noreply.github.com>
2023-08-04 10:18:22 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var about = document.getElementById("about");
+  var home = document.getElementById("home");
+  var memberBtn = document.getElementById("memberBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+  var projectsBtn = document.getElementById("projectsBtn");
+  var contactBtn = document.getElementById("contactBtn");
+  var aboutBtn = document.getElementById("aboutBtn");
+  var homeBtn = document.getElementById("homeBtn");
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+  about.style.display = "none";
+  home.style.display = "none";
+  memberBtn.style.color = "#fff";
+  skillsBtn.style.color = "#000";
+  projectsBtn.style.color = "#000";
+  contactBtn.style.color = "#000";
+  aboutBtn.style.color = "#000";
+  homeBtn.style.color = "#000";
+}
